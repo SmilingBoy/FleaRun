dd61b6025cfed82e1502492801cf6309ecd4475a
SmilingBoy <lizhiqiang@big-mac.lan>
2016-07-11 18:46:40 +0800

    Initial Commit

---
+++ FleaRun/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  FleaRun
+//
+//  Created by 李志强 on 16/7/11.
+//  Copyright © 2016年 lizhiqiang. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
